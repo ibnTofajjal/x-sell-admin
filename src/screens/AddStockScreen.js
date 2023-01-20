3026ae93d995d89583ecca9f83e6f22e099fc608
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2023-01-20 15:16:52 +0600

    Add Simple Selector

--- src/screens/AddStockScreen.js
+++ src/screens/AddStockScreen.js
@@ -1,20 +1,36 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
+import DropDownPicker from "react-native-dropdown-picker";
 import MyTitle from "../components/MyTitle";
 import { Colors } from "../ui/Theme";
 
 const AddStockScreen = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Apple", value: "apple" },
+    { label: "Banana", value: "banana" },
+  ]);
   return (
     <LinearGradient
       colors={[Colors.g1, Colors.g2, Colors.g3]}
       style={{ flex: 1 }}
     >
       <SafeAreaView>
-        <ScrollView>
-          <MyTitle title={"Add Stock"} />
-        </ScrollView>
+        <MyTitle title={"Add Stock"} />
+
+        <View style={styles.dropdownStyle}>
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+          />
+        </View>
       </SafeAreaView>
     </LinearGradient>
   );
@@ -22,4 +38,10 @@ const AddStockScreen = () => {
 
 export default AddStockScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  dropdownStyle: {
+    backgroundColor: "#fafafa",
+    width: 200,
+    alignSelf: "center",
+  },
+});
