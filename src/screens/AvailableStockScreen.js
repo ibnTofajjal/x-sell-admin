61de02e24f5a8c1dc539ab964ebbfa5b82e76db2
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2023-02-06 18:57:23 +0600

    SearchBar implement in Available Stock

--- src/screens/AvailableStockScreen.js
+++ src/screens/AvailableStockScreen.js
@@ -6,7 +6,9 @@ import MyTitle from "../components/MyTitle";
 import AddComponent from "../components/AddComponent";
 import { Colors } from "../ui/Theme";
 import MyInput from "../components/MyInput";
+import { AntDesign } from "@expo/vector-icons";
 import MyButton from "../components/MyButton";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 const AvailableStockScreen = () => {
   return (
@@ -16,7 +18,20 @@ const AvailableStockScreen = () => {
     >
       <SafeAreaView>
         <ScrollView>
-          <MyTitle title={"Available Stock"} />
+          <View style={styles.container}>
+            <MyInput
+              placeholder="Search Your Product"
+              customStyle={styles.searchInput}
+            />
+            <Pressable>
+              <AntDesign
+                name="search1"
+                size={24}
+                color="black"
+                style={styles.search}
+              />
+            </Pressable>
+          </View>
         </ScrollView>
       </SafeAreaView>
     </LinearGradient>
@@ -25,4 +40,41 @@ const AvailableStockScreen = () => {
 
 export default AvailableStockScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+
+  searchInput: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    padding: 15,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+
+  search: {
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    alignSelf: "center",
+    padding: 16,
+    marginTop: 11,
+    marginRight: 15,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+});
